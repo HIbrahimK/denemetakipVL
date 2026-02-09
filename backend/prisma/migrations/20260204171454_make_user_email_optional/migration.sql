0f636ac58436243d7d0e0e7c804c3508704f0922
Halil İbrahim KOCAGÖZ <hikomen@gmail.com>
2026-02-09 11:10:54 +0300

    migration hatası

--- backend/prisma/migrations/20260204171454_make_user_email_optional/migration.sql
+++ backend/prisma/migrations/20260204171454_make_user_email_optional/migration.sql
@@ -1,2 +1,12 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "email" DROP NOT NULL;
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'User'
+      AND column_name = 'email'
+      AND is_nullable = 'NO'
+  ) THEN
+    ALTER TABLE "User" ALTER COLUMN "email" DROP NOT NULL;
+  END IF;
+END $$;
