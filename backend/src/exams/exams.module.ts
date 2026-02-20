392df110a01e97ebb484431d452dd30c49b9e6f1
HIbrahimK <hikomen@gmail.com>
2026-02-20 22:52:58 +0300

    Fix Prisma connection exhaustion by using singleton PrismaService

--- backend/src/exams/exams.module.ts
+++ backend/src/exams/exams.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ExamsController } from './exams.controller';
 import { ExamsService } from './exams.service';
 import { ExamNotificationTask } from './exam-notification.task';
-import { PrismaService } from '../prisma/prisma.service';
 import { ReportsModule } from '../reports/reports.module';
 
 @Module({
-    imports: [ReportsModule],
-    controllers: [ExamsController],
-    providers: [ExamsService, PrismaService, ExamNotificationTask],
-    exports: [ExamsService],
+  imports: [ReportsModule],
+  controllers: [ExamsController],
+  providers: [ExamsService, ExamNotificationTask],
+  exports: [ExamsService],
 })
 export class ExamsModule { }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController, ProfileController } from './users.controller';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [UsersController, ProfileController],
-    providers: [UsersService, PrismaService],
-    exports: [UsersService],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule { }
